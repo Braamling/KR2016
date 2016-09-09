cfeb88fdc2d9f0582553daa7a05d1bf9f846ead7
Braamling <contact@bramvandenakker.nl>
2016-09-09 12:16:03 +0200

    main file comments

--- Solver/main.py
+++ Solver/main.py
@@ -9,15 +9,17 @@ from sudoku_generator import SudokuGenerator
 
 
 if __name__ == '__main__':
-    generator = SudokuGenerator('medium')
+    # Create a Sudoku generator instance for medium difficult sudokus.
+    generator = SudokuGenerator('hard')
 
+    # Generate a medium difficult sudoku and convert to an array
     sudoku = generator.get_sudoku().get_array()
 
-    pprint(sudoku)
-
+    # Shuffle the row's of the sudoku to create a regionless sudoku
     random.shuffle(sudoku)
 
-    pprint(sudoku)
-
+    # Solve the Sudoku
     solver.solve(sudoku)
+
+    # Print the sudoku solution.
     pprint(sudoku)
