1a800d0a3c3d86cd4335a11c3b86de9509e95e09
Braamling <contact@bramvandenakker.nl>
2016-09-09 12:47:29 +0200

    start for iterations

--- Solver/main.py
+++ Solver/main.py
@@ -4,22 +4,35 @@ and solves these with pycosat
 """
 import solver
 import random
+import time
+
+import matplotlib.pyplot as plt
+
 from pprint import pprint
 from sudoku_generator import SudokuGenerator
 
 
 if __name__ == '__main__':
+    sudoku_results = []
+    no_region_results = []
+
     # Create a Sudoku generator instance for medium difficult sudokus.
-    generator = SudokuGenerator('hard')
+    generator = SudokuGenerator('medium')
 
-    # Generate a medium difficult sudoku and convert to an array
-    sudoku = generator.get_sudoku().get_array()
+    # Take computational start time
+    start_normal = time.time()
+    for x in xrange(0, 10000):
+        # Generate a medium difficult sudoku and convert to an array
+        sudoku = generator.get_sudoku().get_array()
 
-    # Shuffle the row's of the sudoku to create a regionless sudoku
-    random.shuffle(sudoku)
+        # Solve the Sudoku with a region rule applied
+        solver.solve(sudoku, with_region_rule=True)
 
-    # Solve the Sudoku
-    solver.solve(sudoku)
+        # Shuffle the row's of the sudoku to create a regionless sudoku
+        random.shuffle(sudoku)
+
+        # Solve the shuffled sudoku
+        solver.solve(sudoku, with_region_rule=False)
 
     # Print the sudoku solution.
     pprint(sudoku)
