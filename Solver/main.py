c6600c86e359a69eb5b7987d705c5955d1602d9e
Braamling <contact@bramvandenakker.nl>
2016-09-09 11:57:59 +0200

    Sudoku + generator loop

--- Solver/main.py
+++ Solver/main.py
@@ -10,16 +10,9 @@ from sudoku_generator import SudokuGenerator
 if __name__ == '__main__':
     generator = SudokuGenerator('medium')
 
-    print generator.get_sudoku()
+    sudoku = generator.get_sudoku().get_array()
 
-    hard = [[0, 2, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 6, 0, 0, 0, 0, 3],
-            [0, 7, 4, 0, 8, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 3, 0, 0, 2],
-            [0, 8, 0, 0, 4, 0, 0, 1, 0],
-            [6, 0, 0, 5, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 7, 8, 0],
-            [5, 0, 0, 0, 0, 9, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 4, 0]]
-    solver.solve(hard)
-    pprint(hard)
+    pprint(sudoku)
+
+    solver.solve(sudoku)
+    pprint(sudoku)
