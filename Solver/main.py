8eeaa8fbcd717b6825a18b0a2d43f4a44c6ae30e
Haitam <haitam_benyahia@hotmail.com>
2016-09-09 11:34:26 +0200

    Main script and generator script files made

--- Solver/main.py
+++ Solver/main.py
@@ -2,9 +2,21 @@
 The main script that generates sudokus with and without region rules and solves these with pycosat
 """
 import solver
+from pprint import pprint
 import generator
 
 
 if __name__ == '__main__':
-    new_sudoku_example = generator.generate() # change this later to a loop
-    solver.solve(new_sudoku_example)
+    #new_sudoku_example = generator.generate() # change this later to a loop
+
+    hard = [[0, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 6, 0, 0, 0, 0, 3],
+            [0, 7, 4, 0, 8, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 3, 0, 0, 2],
+            [0, 8, 0, 0, 4, 0, 0, 1, 0],
+            [6, 0, 0, 5, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 7, 8, 0],
+            [5, 0, 0, 0, 0, 9, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 4, 0]]
+    solver.solve(hard)
+    pprint(hard)
